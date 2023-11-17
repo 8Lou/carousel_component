840204ac5e922a0eeeb0b31421b6b7df191f589f
8Lou <lou8@mail.ru>
2023-11-17 16:56:32 +0300

    add_Carousel_component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello!
+    <div className={styles.container}>
+      <h1>Carousel demo</h1>
+      <div className={styles.wrapper}>
+        <div className={styles.carouselContainerOne}>
+          
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Carousel/Carousel.js
@@ -0,0 +1,13 @@
+
+import styles from './Carousel.module.css';
+
+const Carousel = ({ images }) => {
+
+  return (
+    <div className={styles.carouselContainer}>
+      
+    </div>
+  );
+};
+
+export default Carousel;
